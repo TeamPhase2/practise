32293c1cd98172d241b1d19eea8ff49416b35850
PushpaSatvika <92777679+PushpaSatvika@users.noreply.github.com>
2022-01-06 17:01:41 +0530

    Changed the variable Names as per user Understandable

--- Java8EntireCourse/src/practice/Practice.java
+++ Java8EntireCourse/src/practice/Practice.java
@@ -9,23 +9,23 @@ import java.util.stream.Collectors;
 
 public class Practice {
     public static void main(String args[]) {
-        List<Employee> list = new ArrayList<>();
-        list.add(new EmpolyeeBuilder().setId(1).setName("Pushpa").setAddress("Hyderabad").setColor("pink").build());
-        list.add(new EmpolyeeBuilder().setId(1).setName("Vijaya").setAddress("Hyderabad").setColor("pink").build());
-        list.add(new EmpolyeeBuilder().setId(1).setName("Akhil").setAddress("chennai").setColor("Brown").build());
-        list.add(new EmpolyeeBuilder().setId(1).setName("Pranith").setAddress("chennai").setColor("Black").build());
-        list.add(new EmpolyeeBuilder().setId(1).setName("Vinay").setAddress("Hyderabad").setColor("Blue").build());
+        List<Employee> employeeList = new ArrayList<>();
+        employeeList.add(new EmpolyeeBuilder().setId(1).setName("Pushpa").setAddress("Hyderabad").setColor("pink").build());
+        employeeList.add(new EmpolyeeBuilder().setId(1).setName("Vijaya").setAddress("Hyderabad").setColor("pink").build());
+        employeeList.add(new EmpolyeeBuilder().setId(1).setName("Akhil").setAddress("chennai").setColor("Brown").build());
+        employeeList.add(new EmpolyeeBuilder().setId(1).setName("Pranith").setAddress("chennai").setColor("Black").build());
+        employeeList.add(new EmpolyeeBuilder().setId(1).setName("Vinay").setAddress("Hyderabad").setColor("Blue").build());
 
         //Find the given employee address belongs to Hyderabad or not
-        Predicate<Employee>predicate=t->t.getAddress().equalsIgnoreCase("hyderabad");
-        list.stream().filter(predicate).map(Employee::getName).forEach(System.out::println);
+        Predicate<Employee>addresspredicate=t->t.getAddress().equalsIgnoreCase("hyderabad");
+        list.stream().filter(addresspredicate).map(Employee::getName).forEach(System.out::println);
         //a list of colors and find the given color is in the list or not
 
 
         System.out.println("--------------------------------------------------------------------------------------");
 
-        Predicate<Employee>predicate1=t->t.getColor().equalsIgnoreCase("pink");
-        list.stream().filter(predicate1).map(Employee::getName).collect(Collectors.toList()).forEach(System.out::println);
+        Predicate<Employee>colorPredicate=t->t.getColor().equalsIgnoreCase("pink");
+        list.stream().filter(colorPredicate).map(Employee::getName).collect(Collectors.toList()).forEach(System.out::println);
 
     }
 }
