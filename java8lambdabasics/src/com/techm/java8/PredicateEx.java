dcd35d2dc7f1b4220067f78803e240372307d387
Akhil-9999 <93604439+Akhil-9999@users.noreply.github.com>
2022-01-06 16:39:35 +0530

    added predicate to find even and list of integers

---
+++ java8lambdabasics/src/com/techm/java8/PredicateEx.java
@@ -0,0 +1,29 @@
+package com.techm.java8;
+
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+
+
+public class PredicateEx {
+
+
+        public static void main(String[] args)
+        {
+            Predicate<Integer>  isEven = number -> (number % 2 )== 0;
+
+
+
+            System.out.println( isEven.test(8) );
+            System.out.println( isEven.test(11) );
+
+            List<Integer> integerlist = Arrays.asList(10,20,30,40);
+            Predicate<Integer> predicateList = i-> integerlist.contains(i);
+            System.out.println(predicateList.test(11));
+
+        }
+    }
+
+
+
