0c03d0eee12665240cfa3414002512d7d9b7fd3d
Guntreddi Venkatesh <40067415+Venkatesh-Guntreddi@users.noreply.github.com>
2022-01-11 15:49:37 +0530

    Update UsingConsumerExample1.java

--- FuctionalInterfaces/UsingConsumerExample1.java
+++ FuctionalInterfaces/UsingConsumerExample1.java
@@ -5,23 +5,18 @@ import java.util.List;
 import java.util.function.Consumer;
 
 public class UsingConsumerExample1 {
-	
+
 	public static void main(String[] args) {
-		@Test
-	        
-			List<String> cities = Arrays.asList("Sydney", "New York", "London");
-
-		    Consumer<List<String>> convertToUpperCase = list -> {
-		        for(int i=0; i< list.size(); i++){
-		            list.set(i, list.get(i).toUpperCase());
-		        }
-		    };
-		    
-		    Consumer<List<String>> printCities = list -> list.stream().forEach(System.out::println);
-
-		    convertToUpperCase.andThen(printCities).accept(cities);
-		    
-	} 
+	
+		List<String> Cities = Arrays.asList("Sydney", "New York", "London");
+		
+		Consumer<List<String>> printCities = CitiesList -> Cities
+				.stream()
+				.map(String::toUpperCase)
+				.forEach(System.out::println);
+				
+				printCities.accept(Cities);
+	}
 
 }
 
