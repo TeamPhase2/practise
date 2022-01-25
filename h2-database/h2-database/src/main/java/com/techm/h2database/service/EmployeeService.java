06ecbc88b0cd88886fb52dca9775cb067b2db789
vijayalakshmi2428 <92805568+vijayalakshmi2428@users.noreply.github.com>
2022-01-25 18:44:27 +0530

    small change added

--- h2-database/h2-database/src/main/java/com/techm/h2database/service/EmployeeService.java
+++ h2-database/h2-database/src/main/java/com/techm/h2database/service/EmployeeService.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 @Service
 public class EmployeeService {
@@ -16,14 +18,33 @@ public class EmployeeService {
     @Autowired
     EmployeeRepository employeeRepository;
 
-    public Employee saveEmployee(Employee employee){
-        return employeeRepository.save(employee);
+    @Value("${employee.employeelist.details}")
+    private List<String> employeeNameList;
 
+    public List<Employee> stringListToEmployeeListConversion() {
+        List<Employee> employeeList = new ArrayList<>();
+        for (String s : employeeNameList) {
+            Employee e = new Employee();
+            e.setName(s);
+            employeeList.add(e);
+        }
+        return employeeList;
     }
 
-    public List<Employee> getAllEmployees(){
+    public List<Employee> getAllEmployees() {
+        List<Employee> employees = new ArrayList<>();
+        employeeRepository.findAll().forEach(employee -> employees.add(employee));
+        return employees;
+    }
+
+    public Employee getEmployeeById(int id) {
+        return employeeRepository.findById(id).get();
+    }
+
+    public List<Employee> saveOrUpdate() {
         List<Employee> employeeList = new ArrayList<>();
-      employeeRepository.findAll().forEach(employees -> employeeList.add(employees));
-      return employeeList;
+        employeeNameList.stream().map(e -> employeeList.add(new Employee(null, e,"Hyderabad","25000"))).collect(Collectors.toList());
+        employeeRepository.saveAll(employeeList).forEach(employee1 -> employeeRepository.save(employee1));
+        return employeeList;
     }
 }
