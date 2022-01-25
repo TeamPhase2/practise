b3560f89ccb897435249ca860151caef9a58f4aa
vijayalakshmi2428 <92805568+vijayalakshmi2428@users.noreply.github.com>
2022-01-25 18:41:46 +0530

    small changes added

--- h2-database/h2-database/src/main/java/com/techm/h2database/controller/EmployeeController.java
+++ h2-database/h2-database/src/main/java/com/techm/h2database/controller/EmployeeController.java
@@ -3,14 +3,13 @@ package com.techm.h2database.controller;
 import com.techm.h2database.entity.Employee;
 import com.techm.h2database.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
+
 
 @RestController
 public class EmployeeController {
@@ -18,26 +17,19 @@ public class EmployeeController {
     @Autowired
     EmployeeService employeeService;
 
-    @Value("${employee.employeelist.details}")
-    private String stringList;
-
-    public Employee map(){
-        String[] tokens = stringList.split(",");
-        Employee result = new Employee(Integer.parseInt(tokens[0]),tokens[1],tokens[2],tokens[3]);
-        return result;
-    }
-
     @PostMapping("/saveEmployee")
-    public List<Employee> addEmployee(@RequestBody Employee employee){
-        Employee employees = map();
-        List<Employee> employeeList= new ArrayList<>();
-        employeeList.add(employeeService.saveEmployee(employee));
-        employeeList.add(employeeService.saveEmployee(employees));
-         return employeeList ;
+    public List<Employee> saveEmployee() {
+        return employeeService.saveOrUpdate();
     }
 
-    @GetMapping("/employees")
-    public List<Employee> getEmployees(){
+    @GetMapping("/getAllEmployees")
+    public List<Employee> getAll() {
         return employeeService.getAllEmployees();
     }
+
+    @GetMapping("/employee/{id}")
+    public Employee getEmployee(@PathVariable("id") int id) {
+        return employeeService.getEmployeeById(id);
+    }
+
 }
